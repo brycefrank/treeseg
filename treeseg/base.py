7788b8788a26e083acc615e4fd6a9bf15d3e74ca
brycefrank <bryce.frank@oregonstate.edu>
2019-04-22 16:49:40 -0700

    EOD push

--- treeseg/base.py
+++ treeseg/base.py
@@ -4,17 +4,21 @@ class HeightModel:
     """
     The base class for all rasterized height models used for detection and segmentation.
     """
-
     def __init__(self, array, crs=None, affine=None):
+        self.crs = crs
+        if self.crs is not None:
+            self._check_crs()
+
+        self.array = array
+        self.affine = affine
+
+    def _check_crs(self):
         try:
-            Proj(crs)
+            Proj(self.crs)
         except RuntimeError:
             import warnings
             warnings.warn('Invalid projection string. Proceeding as usual, but expect errors in georeferenced outputs.')
 
-        self.array = array
-        self.crs = crs
-        self.affine = affine
 
     @classmethod
     def from_raster(cls, raster_obj):
@@ -22,6 +26,9 @@ class HeightModel:
 
     @classmethod
     def from_tif(cls):
+        pass
+
+    def plot(self):
         pass
 
 class SegmentBase:
--- treeseg/segmentation.py
+++ treeseg/segmentation.py
@@ -1,9 +1,14 @@
-from treeseg.base import HeightModel
+from treeseg.base import HeightModel, SegmentBase
 
-class Watershed(HeightModel):
+class Watershed(SegmentBase):
     """
     A wrapper for :func:`skimage.morphology.watershed`.
     """
 
     def __init__(self, height_model):
         pass
+
+    @property
+    def segments(self):
+        pass
+
